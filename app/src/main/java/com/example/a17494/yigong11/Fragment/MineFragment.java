615808834d572da79f85e0dfb7a90e7d8564cc91
wxp109761 <47017857+wxp109761@users.noreply.github.com>
2019-05-22 23:40:18 +0800

    头像更换功能

--- app/src/main/java/com/example/a17494/yigong11/Fragment/MineFragment.java
+++ app/src/main/java/com/example/a17494/yigong11/Fragment/MineFragment.java
@@ -1,7 +1,16 @@
 package com.example.a17494.yigong11.Fragment;
-
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,13 +27,22 @@ import com.example.a17494.yigong11.Activity.MyMessActivity;
 import com.example.a17494.yigong11.Activity.SettingActivity;
 import com.example.a17494.yigong11.Activity.UserInfoActivity;
 import com.example.a17494.yigong11.Bean.HourBean;
+import com.example.a17494.yigong11.MainActivity;
 import com.example.a17494.yigong11.R;
 import com.example.a17494.yigong11.Utils.Constants;
 import com.example.a17494.yigong11.Utils.SpUtils;
 import com.example.a17494.yigong11.cookie.RetrofitClient;
 import com.flyco.dialog.listener.OnOperItemClickL;
 import com.flyco.dialog.widget.ActionSheetDialog;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import rx.Subscriber;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class MineFragment extends BaseFragment implements View.OnClickListener{
@@ -38,6 +56,16 @@ public class MineFragment extends BaseFragment implements View.OnClickListener{
     private TextView user_info;
     private TextView mCollect;
     private TextView mAllHour;
+
+
+
+    private Bitmap head;// 头像Bitmap
+    private static String path = "/sdcard/myHead/";// sd路径
+
+    //调用照相机返回图片文件
+    private File tempFile;
+
+
     @Override
     protected void setSubListenter() {
 
@@ -45,6 +73,10 @@ public class MineFragment extends BaseFragment implements View.OnClickListener{
     @Override
     protected View getSubView(LayoutInflater inflater, ViewGroup container) {
         rootView=inflater.inflate(R.layout.mine_view_fg,container,false);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+            StrictMode.setVmPolicy(builder.build());
+        }
         initView();
         return rootView;
     }
@@ -68,6 +100,18 @@ public class MineFragment extends BaseFragment implements View.OnClickListener{
         settings.setOnClickListener(this);
         Log.d("GGG",SpUtils.getString(getContext(),Constants.STU_NAME)+"");
         userName.setText(SpUtils.getString(getContext(),Constants.STU_NAME));
+
+        Bitmap bt = BitmapFactory.decodeFile(path + "head.jpg");// 从SD卡中找头像，转换成Bitmap
+        if (bt != null) {
+            @SuppressWarnings("deprecation")
+            Drawable drawable = new BitmapDrawable(bt);// 转换成drawable
+            headerImg.setImageDrawable(drawable);
+        } else {
+            /**
+             * 如果SD里面没有则需要从服务器取头像，取回来的头像再保存在SD中
+             *
+             */
+        }
     }
     @Override
     public void onClick(View v) {
@@ -111,16 +155,20 @@ public class MineFragment extends BaseFragment implements View.OnClickListener{
         final ActionSheetDialog dialog = new ActionSheetDialog(rootView.getContext(), stringItems, null);
         dialog.isTitleShow(true).show();
         dialog.title("更换头像");
-        dialog.itemPressColor(Color.parseColor("#e9857d"));
+        dialog.itemTextColor(Color.parseColor("#e9857d"));
+        dialog.cancelText(Color.parseColor("#e9857d"));
+       // dialog.itemPressColor(Color.parseColor("#e9857d"));
         dialog.setOnOperItemClickL(new OnOperItemClickL() {
             @Override
             public void onOperItemClick(AdapterView<?> parent, View view, int position, long id) {
                 switch (position) {
                     case 0:
-                        Toast.makeText(rootView.getContext(),"更换头像",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(rootView.getContext(),"拍照",Toast.LENGTH_SHORT).show();
+                        initCamera();
                         break;
                     case 1:
                         Toast.makeText(rootView.getContext(),"从相册选择",Toast.LENGTH_SHORT).show();
+                        initLocal();
                         break;
                 }
                 dialog.dismiss();
@@ -173,4 +221,121 @@ public class MineFragment extends BaseFragment implements View.OnClickListener{
             }
         },SpUtils.getString(getContext(),Constants.STU_ID));
     }
+
+
+
+    public void initCamera() {
+
+        //最好用try/catch包裹一下，防止因为用户未给应用程序开启相机权限，而使程序崩溃
+        try {
+            Intent intent2 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);//开启相机应用程序获取并返回图片（capture：俘获）
+            intent2.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(Environment.getExternalStorageDirectory(),
+                    "head.jpg")));//指明存储图片或视频的地址URI
+            startActivityForResult(intent2, 2);//采用ForResult打开
+        } catch (Exception e) {
+            Toast.makeText(rootView.getContext(), "相机无法启动，请先开启相机权限", Toast.LENGTH_LONG).show();
+        }
+    }
+    public void initLocal() {
+        Intent intent1 = new Intent(Intent.ACTION_PICK, null);//返回被选中项的URI
+        intent1.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");//得到所有图片的URI
+//                System.out.println("MediaStore.Images.Media.EXTERNAL_CONTENT_URI  ------------>   "
+//                        + MediaStore.Images.Media.EXTERNAL_CONTENT_URI);//   content://media/external/images/media
+        startActivityForResult(intent1, 1);
+    }
+
+
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            //从相册里面取相片的返回结果
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    cropPhoto(data.getData());//裁剪图片
+                }
+
+                break;
+            //相机拍照后的返回结果
+            case 2:
+                if (resultCode == RESULT_OK) {
+                    File temp = new File(Environment.getExternalStorageDirectory()
+                            + "/head.jpg");
+                    cropPhoto(Uri.fromFile(temp));//裁剪图片
+                }
+
+                break;
+            //调用系统裁剪图片后
+            case 3:
+                if (data != null) {
+                    Bundle extras = data.getExtras();
+                    head = extras.getParcelable("data");
+                    if (head != null) {
+                        /**
+                         * 上传服务器代码
+                         */
+                        setPicToView(head);//保存在SD卡中
+                        headerImg.setImageBitmap(head);//用ImageView显示出来
+                    }
+                }
+                break;
+            default:
+                break;
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    ;
+
+    /**
+     * 调用系统的裁剪
+     *
+     * @param uri
+     */
+    public void cropPhoto(Uri uri) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        //找到指定URI对应的资源图片
+        intent.setDataAndType(uri, "image/*");
+        intent.putExtra("crop", "true");
+        // aspectX aspectY 是宽高的比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        // outputX outputY 是裁剪图片宽高
+        intent.putExtra("outputX", 150);
+        intent.putExtra("outputY", 150);
+        intent.putExtra("return-data", true);
+
+        //进入系统裁剪图片的界面
+        startActivityForResult(intent, 3);
+    }
+
+    private void setPicToView(Bitmap mBitmap) {
+        String sdStatus = Environment.getExternalStorageState();
+        if (!sdStatus.equals(Environment.MEDIA_MOUNTED)) { // 检测sd卡是否可用
+            return;
+        }
+        FileOutputStream b = null;
+        File file = new File(path);
+        file.mkdirs();// 创建以此File对象为名（path）的文件夹
+        String fileName = path + "head.jpg";//图片名字
+        try {
+            b = new FileOutputStream(fileName);
+            mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, b);// 把数据写入文件（compress：压缩）
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                //关闭流
+                b.flush();
+                b.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
 }
